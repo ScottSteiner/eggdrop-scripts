e32c3bd05c034601025e32e48cde667faaf9acfd
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2014-01-09 03:45:11 -0500

    Newline cleanup

--- steiner-fortune.tcl
+++ steiner-fortune.tcl
@@ -21,7 +21,7 @@ namespace eval steiner {
 	proc public {nick uhost hand chan arg} {
 		if {[string tolower [lindex [split $arg] 0]] == "-p"} { set targ "$nick";set arg "[lindex [split $arg] 1]"; } else { set targ "$chan" }
 		if {$arg == ""} { set fnick $nick } else { set fnick $arg }
-		if {[channel get $chan steiner] && [channel get $chan fortune]} { puthelp "PRIVMSG $targ :[fortune $targ $fnick]" }
+		if {[channel get $chan steiner] && [channel get $chan fortune]} { puthelp "PRIVMSG $targ :[fortune $targ $fnick]" } else { puthelp "PRIVMSG $nick :[fortune $targ $fnick]" }
 	}
 
 	proc fortune { targ nick } {
@@ -29,7 +29,7 @@ namespace eval steiner {
 			if {([dict exists $steiner::fortune::cache $nick time]) && ([clock format [clock seconds] -format "%Y-%m-%d"] == [dict get $steiner::fortune::cache $nick time])} {
 				return "[dict get $steiner::fortune::cache $nick output]"
 			}
-			catch { exec $steiner::settings::fortune::path {*}$steiner::settings::fortune::options | tr {\n\t} { } } output
+			catch { exec $steiner::settings::fortune::path {*}$steiner::settings::fortune::options | sed "s/--.*//" | tr {\n\t} { } } output
 			set output "$nick's fortune for today is: $output"
 			dict set steiner::fortune::cache $nick output $output
 			dict set steiner::fortune::cache $nick time [clock format [clock seconds] -format "%Y-%m-%d"]
