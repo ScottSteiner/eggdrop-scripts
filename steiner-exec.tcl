7941056efb1708a5f1ba6e313f7b6b3b2028d655
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2014-02-24 02:20:21 -0500

    Times function: Customizable function to show times around the world.

--- steiner-exec.tcl
+++ steiner-exec.tcl
@@ -1,10 +1,10 @@
 #########################################################################################
 # Name			steiner:exec
 # Description		Executes a command on your system's shell.  Only works for people with the correct flags
-# Version		1.1 (2012-04-23)
+# Version		1.2 (2014-02-24)
 # Contact		ScottSteiner@irc.rizon.net
 # Website		https://github.com/ScottSteiner/eggdrop-scripts
-# Copyright		2010-2012, ScottSteiner <nothingfinerthanscottsteiner@gmail.com>
+# Copyright		2010-2014, ScottSteiner <nothingfinerthanscottsteiner@gmail.com>
 # License		GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 #########################################################################################
 if {[catch {source scripts/steiner-settings.tcl} err]} {
@@ -14,7 +14,7 @@ if {[catch {source scripts/steiner-settings.tcl} err]} {
 namespace eval steiner {
    namespace eval exec {
 	set flag $steiner::settings::exec::flag
-	if {$steiner::settings::exec::enablepublic} { foreach cmd {date stats sysinfo uname uptime} { bind pub ${flag}|${flag} ${steiner::settings::prefix}$cmd steiner::exec::public } }
+	if {$steiner::settings::exec::enablepublic} { foreach cmd {date stats sysinfo times uname uptime} { bind pub ${flag}|${flag} ${steiner::settings::prefix}$cmd steiner::exec::public } }
 
 	proc public {nick uhost hand chan arg} {
 		if {[string tolower [lindex [split $arg] 0]] == "-p"} { set targ "$nick" } else { set targ "$chan" }
@@ -24,9 +24,13 @@ namespace eval steiner {
 
 	proc execute { targ cmd } {
 		if {$cmd == "date"}	{ set cmd "date +%F\\ %T%:::z" }
+		if {$cmd == "stats"}	{ puthelp "PRIVMSG $targ :Channel stats can be found at http://scottsteiner.github.io"; return 1 }
 		if {$cmd == "sysinfo"}	{ set cmd "/usr/local/bin/sysinfo" }
+
+		#Time zone listings can be found at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+		if {$cmd == "times"}	{ puthelp "PRIVMSG $targ :[clock format [clock seconds] -format {Los Angeles %I:%M %p %Z} -timezone America/Los_Angeles] | [clock format [clock seconds] -format {New York %I:%M %p %Z} -timezone America/New_York] |\
+								[clock format [clock seconds] -format {London %I:%M %p %Z} -timezone Europe/London] | [clock format [clock seconds] -format {Berlin %I:%M %p %Z} -timezone Europe/Berlin] | [clock format [clock seconds] -format {Tokyo %I:%M %p %Z} -timezone Asia/Tokyo]"; return 1 }
 		if {$cmd == "uname"}	{ set cmd "uname -a" }
-		if {$cmd == "stats"}	{ puthelp "PRIVMSG $targ :Channel stats can be found at http://scottsteiner.is-great.org/stats.php"; return 1 }
 		catch { eval exec $cmd } output
 		puthelp "PRIVMSG $targ :$output"
 	}
