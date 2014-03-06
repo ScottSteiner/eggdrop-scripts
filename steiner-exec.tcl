a403a6e6ce6df99870a491224f6f7e03fb6bf307
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2014-03-05 23:35:32 -0500

    Reorganized binds. condensed code

--- steiner-exec.tcl
+++ steiner-exec.tcl
@@ -1,7 +1,7 @@
 #########################################################################################
 # Name			steiner:exec
 # Description		Executes a command on your system's shell.  Only works for people with the correct flags
-# Version		1.2 (2014-02-24)
+# Version		1.2.1 (2014-03-05)
 # Contact		ScottSteiner@irc.rizon.net
 # Website		https://github.com/ScottSteiner/eggdrop-scripts
 # Copyright		2010-2014, ScottSteiner <nothingfinerthanscottsteiner@gmail.com>
@@ -19,25 +19,25 @@ namespace eval steiner {
 	proc public {nick uhost hand chan arg} {
 		if {[string tolower [lindex [split $arg] 0]] == "-p"} { set targ "$nick" } else { set targ "$chan" }
 		set cmd [string range [lindex $::lastbind 0] 1 end]
-		execute $targ $cmd
+		puthelp "PRIVMSG $targ :[execute $cmd]"
 	}
 
-	proc execute { targ cmd } {
+	proc execute { cmd } {
 		if {$cmd == "date"}	{ set cmd "date +%F\\ %T%:::z" }
-		if {$cmd == "stats"}	{ puthelp "PRIVMSG $targ :Channel stats can be found at http://scottsteiner.github.io"; return 1 }
 		if {$cmd == "sysinfo"}	{ set cmd "/usr/local/bin/sysinfo" }
+		if {$cmd == "uname"}	{ set cmd "uname -a" }
+
+		if {$cmd == "stats"}	{ return "Channel stats can be found at http://scottsteiner.github.io" }
 		if {$cmd == "times"}	{
 			foreach city [dict keys $steiner::settings::exec::times] {
 				set timezone [dict get $steiner::settings::exec::times $city]
 				lappend info "$city [clock format [clock seconds] -format {%I:%M %p %Z} -timezone $timezone]"
 			}
 			set output [join $info " | "]
-			puthelp "PRIVMSG $targ :$output"
-			return 1
+			return "$output"
 		}
-		if {$cmd == "uname"}	{ set cmd "uname -a" }
 		catch { eval exec $cmd } output
-		puthelp "PRIVMSG $targ :$output"
+		return "$output"
 	}
    }
 }
